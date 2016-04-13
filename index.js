3c9ba6efa1c08cb99dd8d43013fb83667c5bd378
Matthew Barben <matthew.barben@anvilation.com>
2016-04-12 20:24:20 -0700

    Version 0.1.0 Commit
     - Added Logging (utilising Winston)
     - Corrected issue with the prgram deleting the source document too early,
       will not monitor output folder and then remove the source document once
       it has been sucessfully rendered
     - added state varible which will monitor the state of the documents as
       they go through the program

--- index.js
+++ index.js
@@ -1,43 +1,116 @@
 'use strict'
 
-
+// **************************************************
+// Constants
+// **************************************************
 const winston = require('winston')
 const fs = require ('fs')
 const path = require ('path')
 const chokidar = require('chokidar')
 const exec = require('child_process').exec
 
-const config = require('./configuration')
+// **************************************************
+// Create PDF Program
+// **************************************************
+
+const config = require('./configuration') // load configuration
+var applicationLog = config.log + '/createpdf.log'
+var state =  [] // this will check the state of the application
+var logger = new (winston.Logger)({ // setup logger
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: applicationLog})
+    ]
+})
+logger.log('info', `Debug Logging is set to ${config.debug}`)
+logger.log('info', `Input Folder set to ${config.input}`)
+logger.log('info', `Output Folder set to ${config.output}`)
+logger.log('info', `Location of Libre Office is ${config.libre}`)
+
+config.debug ? (
+		logger.transports.console.level = 'debug',
+		logger.transports.file.level = 'debug'
+	):(
+		logger.transports.console.level = 'error',
+		logger.transports.file.level = 'error'
+	
+	);
 
 
-var watcher = chokidar.watch(config.input, {
+// **************************************************
+// Watching Input Folder
+// **************************************************
+var inputWatch = chokidar.watch(config.input, {
     ignored: /[\/\\]\./,
     persistent:true
 })
 
-watcher
+inputWatch
  .on('add', folderPath => {
-    console.log(`stuff ${folderPath} has been added`)
-    //C:\LibreOffice\program\soffice -headless -invisible --convert-to pdf .\sampleDocument.docx -outdir c:/test
-    console.log(`${config.libre} -headless -invisible --convert-to pdf ${folderPath} -outdir ${config.output}`)
+    logger.log('debug', `${folderPath} has been added`)
+    let fileExtention = path.extname(folderPath)
+    let fileName = path.basename(folderPath, fileExtention)
+    logger.log('debug', `File Extention is ${fileExtention}, document name is ${fileName}`)
+    let document ={
+        fileName: fileName,
+        folderPath: folderPath,
+        fileExtention: fileExtention
+    }
+    state.push(document)
+    logger.log('debug', `State update to  ${JSON.stringify(state)}`)
+
+    logger.log('debug', `${config.libre} -headless -invisible --convert-to pdf ${folderPath} -outdir ${config.output}`)
     exec(`${config.libre} -headless -invisible --convert-to pdf ${folderPath} -outdir ${config.output}`, (err, stdout, sterr) => {
         if(err){
             throw err
         }
-        console.log(stdout)
+        logger.log('debug', stdout)
     })
-    fs.unlink(folderPath,  (err) => {
-        if(err){
-            throw err
-        } 
-        console.log(`${folderPath} was deleted from input folder`)
-    })
-    
-    
+   
 })
  .on('error', error => {
-     console.log(`An error occured with ${error}`)
+     logger.log('info', `An error occured with ${error}`)
  })
  .on('ready', () => {
-     console.log(`Lets Boogiie`)
+     logger.log('info', `Start Monitoring Input Folder`)
  })
+
+
+// **************************************************
+// Watching Output Folder
+// ************************************************** 
+var outputWatcher =  chokidar.watch(config.output, {
+    ignored: /[\/\\]\./,
+    persistent:true
+})
+ .on('add', folderPath => {
+    logger.log('debug', `${folderPath} has been added`)
+    let fileName = path.basename(folderPath, '.pdf')
+    let stateLength = state.length
+    let index = 0
+    for (index = 0; index < stateLength; index++) {
+        let isDocument = false;
+        fileName === state[index].fileName ? removeDocument(state[index].folderPath) : isDocument  
+        
+    }
+  
+})
+ .on('unlink', folderPath => {
+    logger.log('debug', `${folderPath} has been removed`)
+})
+ .on('error', error => {
+     logger.log('info', `An error occured with ${error}`)
+     
+ })
+ .on('ready', () => {
+     logger.log('info', 'Monitoring Output Folder')
+ }) 
+ 
+ function removeDocument(folderPath) {
+     fs.unlink(folderPath, (err) => {
+         if(err){
+             throw err
+         }
+         logger.log('debug', `${folderPath} has been removed`)
+     })
+ }
